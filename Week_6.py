9f6ad546c0a2db543046222065cfc6808ef4199d
Plastimytes <magezirichardelijah@gmail.com>
2025-10-07 08:55:03 +0300

    Update Week_6.py

--- Week_6.py
+++ Week_6.py
@@ -2,4 +2,23 @@
 ##Create your class
 coins=100
 add=coins+500
-print(f"{add}")
+print(f"I had {add} coins")
+
+##OOP
+class piggy_bank:
+    def __init__(self, coins):
+     self.coins=coins
+     print(f"i currently have {self.coins}")
+    
+    def add(self, coins_add):
+        if coins_add>0:
+         self.coins+=coins_add
+         print(f"I now have {self.coins} coins")
+        else:
+            print(f"No money")
+     
+    def get_balance(self):
+        return self.coins 
+
+p1=piggy_bank(100)
+p1.add(5300)    
